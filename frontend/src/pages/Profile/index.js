f4a92c483ab28bd6a5a9de3466a4cb2cabf71724
Celso de Oliveira Silva <celsoliveira@gmail.com>
2020-10-22 15:43:01 -0300

    Front-end do app (semana omnistack)

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -1,20 +1,58 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import logoImg from "../../assets/logo.jpg";
-import {Link} from "react-router-dom";
+import {Link, useHistory } from "react-router-dom";
 import {FiPower, FiTrash2} from "react-icons/fi";
 
+import api from "../../services/api";
 import "./styles.css";
 
 export default function Profile (){
     
+    const [incidents, setIncidents] = useState([]);
+    const ongId = localStorage.getItem("ongId");
+    const ongName = localStorage.getItem("ongName");
+    const history = useHistory();
+
+    //Toda vez que o valor da variaveis entre [] mudarem o valor, será executada
+    //a função implementata no primeiro parametro de useEffect
+    useEffect( () => {
+        api.get("profile", {
+            headers: {
+                Authorization: ongId
+            }
+        }).then(response => {
+            setIncidents(response.data);
+        })
+    }, [ongId]);
+
+    async function handleDeleteIncident(id){
+        try {
+            await api.delete(`incidents/${id}`, {
+                headers: {
+                    Authorization: ongId
+                }
+            });
+        }catch(err){
+            alert("Erro ao deletar caso, tente novamente.");
+        }
+        //remove o incidente da lista
+        setIncidents(incidents.filter(incident => incident.id != id));
+    }
+
+    async function handleLogout(){
+        localStorage.clear();
+        history.push("/");
+    }
+
     return (
         <div className="profile-container">
             <header>
                 <img src={logoImg} alt="Be the Hero"/>
-                <span>Bem vinda, APAD</span>
+                <span>Bem vinda, {ongName}</span>
 
                 <Link to="/incidents/new" className="button">Cadastrar novo caso</Link>
-                <button type="button">
+                
+                <button type="button" onClick={handleLogout}>
                     <FiPower size={18} color="#e02041"/>
                 </button>
             </header>
@@ -22,63 +60,26 @@ export default function Profile (){
             <h1>Casos cadastrados</h1>
 
             <ul>
-                <li>
+                {incidents.map(incident => (
+                <li key={incident.id}>
+
                     <strong>CASO:</strong>
-                    <p>Caso teste</p>
+                    <p>{incident.title}</p>
 
                     <strong>DESCRIÇÃO:</strong>
-                    <p>Descricao teste</p>
+                    <p>{incident.description}</p>
 
                     <strong>VALOR:</strong>
-                    <p>R4 120,00</p>
+                    <p>{Intl.NumberFormat("pt-BR", { style: "currency", currency: "BRL"}).format(incident.value)}</p>
 
-                    <button type="button"><FiTrash2 size={20} color="a8a8b3"/></button>
+                    <button type="button" onClick={() => handleDeleteIncident(incident.id)}><FiTrash2 size={20} color="a8a8b3"/></button>
                 </li>
 
-                <li>
-                    <strong>CASO:</strong>
-                    <p>Caso teste</p>
-
-                    <strong>DESCRIÇÃO:</strong>
-                    <p>Descricao teste</p>
-
-                    <strong>VALOR:</strong>
-                    <p>R4 120,00</p>
-
-                    <button type="button"><FiTrash2 size={20} color="a8a8b3"/></button>
-                </li>
-
-                <li>
-                    <strong>CASO:</strong>
-                    <p>Caso teste</p>
-
-                    <strong>DESCRIÇÃO:</strong>
-                    <p>Descricao teste</p>
-
-                    <strong>VALOR:</strong>
-                    <p>R4 120,00</p>
-
-                    <button type="button"><FiTrash2 size={20} color="a8a8b3"/></button>
-                </li>
-
-                <li>
-                    <strong>CASO:</strong>
-                    <p>Caso teste</p>
-
-                    <strong>DESCRIÇÃO:</strong>
-                    <p>Descricao teste</p>
-
-                    <strong>VALOR:</strong>
-                    <p>R4 120,00</p>
-
-                    <button type="button"><FiTrash2 size={20} color="a8a8b3"/></button>
-                </li>
-
+                ))}
 
             </ul>
 
-
-
+     
         </div>
 
     );
--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -3,6 +3,7 @@ import React from "react";
 import Logon from "./pages/Logon";
 import Register from "./pages/Register";
 import Profile from "./pages/Profile";
+import NewIncident from "./pages/NewIncident"
 
 export default function Routes(){
     return (
@@ -11,6 +12,7 @@ export default function Routes(){
                 <Route path="/" component={Logon} exact></Route> 
                 <Route path="/register" component={Register}></Route>
                 <Route path="/profile" component={Profile}></Route>
+                <Route path="/incidents/new" component={NewIncident}></Route>
             </Switch>
         </BrowserRouter>
     );
